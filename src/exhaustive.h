a953e4f518b6e276c31b622e57b8974ea34bfd0b
Rogério Villafranca <rvillafr@gmail.com>
2019-06-24 22:00:10 -0300

    Fixed headers.

--- src/exhaustive.h
+++ src/exhaustive.h
@@ -1,2 +1,2 @@
-int *wdp(int, int, int, int *, int *, int);
-int *wde(int, int, int, int, int *, int *, int);
+long *wdp(int, int, int, int *, int *, int);
+long *wde(int, int, int, int, int *, int *, int);
